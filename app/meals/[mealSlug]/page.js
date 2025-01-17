fa01b63d97282a037eceecd42b4324a6ecf9a787
gairik <gairikgmandal@gmail.com>
2025-01-17 18:08:57 +0530

    final commit better-sqllite will not work on vercel

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -7,7 +7,7 @@ import { notFound } from "next/navigation";
 export async function generateMetadata({ params }) {
   const para = await params;
   let mealSlug = para.mealSlug;
-  // directly access mealSlug
+  console.log(mealSlug);
   let meal;
   try {
     meal = await getMeal(mealSlug);
